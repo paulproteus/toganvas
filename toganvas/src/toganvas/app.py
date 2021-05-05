8ddab6973bbb7e4e1c666780b16c07fbdde9847e
Asheesh Laroia <asheesh@asheesh.org>
2021-05-04 19:52:56 -0700

    Try to draw a line segment

--- toganvas/src/toganvas/app.py
+++ toganvas/src/toganvas/app.py
@@ -1,26 +1,36 @@
 """
 Toga canvas line
 """
+import math
+
 import toga
+from toga.colors import WHITE, rgb
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
 
 
 class Toganvas(toga.App):
-
     def startup(self):
-        """
-        Construct and show the Toga application.
+        # Main window of the application with title and size
+        self.main_window = toga.MainWindow(title=self.name, size=(148, 250))
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        # Create canvas and draw tiberius on it
+        self.canvas = toga.Canvas(style=Pack(flex=1))
+        box = toga.Box(children=[self.canvas])
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        # Add the content on the main window
+        self.main_window.content = box
+
+        # Draw
+        self.draw_line_segment()
+
+        # Show the main window
         self.main_window.show()
+
+    def draw_line_segment(self):
+        with self.canvas.fill(color=rgb(149, 119, 73)) as head_filler:
+            head_filler.move_to(112, 103)
+            head_filler.line_to(112, 113)
+
 
 
 def main():
