a6f841f1174ccf60a210250766b3fee4e03c9fa0
Asheesh Laroia <asheesh@asheesh.org>
2021-07-21 21:07:43 -0700

    Clean up for PR publication

--- toganvas/src/toganvas/app.py
+++ toganvas/src/toganvas/app.py
@@ -10,11 +10,14 @@ from toga.style import Pack
 
 class Toganvas(toga.App):
     def startup(self):
-        print('hi startup')
+        self._2021_07_21_startup()
+
+    def _2021_07_21_startup(self):
+        # Build two lines that cross each other
         # Main window of the application with title and size
         self.main_window = toga.MainWindow(title=self.name, size=(148, 250))
 
-        # Create canvas and draw tiberius on it
+        # Create canvas
         self.canvas = toga.Canvas(style=Pack(flex=1))
         box = toga.Box(children=[self.canvas])
 
@@ -22,25 +25,16 @@ class Toganvas(toga.App):
         self.main_window.content = box
 
         # Draw
-        self.draw_line_segment()
-        self.draw_other_line_segment()
-        print('hi end')
+        with self.canvas.stroke(line_width=4.0, color=rgb(100, 100, 255)) as stroke:
+            with stroke.closed_path(0, 0) as closed_stroke:
+                closed_stroke.line_to(112, 113)
+
+        with self.canvas.stroke(line_width=3.0, color=rgb(255, 255, 100)) as stroke:
+            with stroke.closed_path(112, 0) as closed_stroke:
+                closed_stroke.line_to(0, 113)
 
         # Show the main window
         self.main_window.show()
-
-    def draw_line_segment(self):
-        print('hi line segment 1')
-        with self.canvas.stroke(line_width=4.0, color=rgb(100, 100, 255), line_dash=[3, 18]) as head_stroker:
-            with head_stroker.closed_path(0, 0) as closed_head:
-                closed_head.line_to(112, 113)
-
-    def draw_other_line_segment(self):
-        print('hi line segment 2')
-        with self.canvas.stroke(line_width=3.0, color=rgb(255, 255, 100)) as head_stroker:
-            with head_stroker.closed_path(112, 0) as closed_head:
-                closed_head.line_to(0, 113)
-
 
 
 def main():
